d1131959c33ecddedc29dd00a4a6b664a98f7243
eminkin97 <eminkin99@gmail.com>
2017-11-27 18:52:24 -0500

    Server stuff for getting room data

--- Hotel.go
+++ Hotel.go
@@ -1,18 +1,71 @@
 package main
 
 import(
-	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"net/http"
+	"database/sql"
 	"log"
+	"fmt"
+	"encoding/json"
 )
 
-func roothandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+var db *sql.DB
+
+type room_entry struct {
+	hotel_id int
+	room_description string
+}
+
+func room_data_fetcher(w http.ResponseWriter, r *http.Request) {
+	var rooms []room_entry
+
+	state := r.URL.Query()["state"]	//gets state query parameter that user passed on GET request
+
+	//gets hotel rooms available in that state
+	rows, err := db.Query("SELECT r.HotelID, r.Description FROM Room r, Hotel h WHERE r.HotelID = h.HotelID and h.State=?", state)
+	if err != nil {
+        	log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+        	var entry room_entry
+        	if err := rows.Scan(&(entry.hotel_id), &(entry.room_description)); err != nil {
+                	log.Fatal(err)
+        	}
+
+		//append room_entry to the rooms array
+		rooms = append(rooms, entry)
+	}
+
+	if err := rows.Err(); err != nil {
+        	log.Fatal(err)
+	}
+
+	json, err := json.Marshal(rooms)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//returns back json response
+	fmt.Printf("At the end\n")
+	
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json)
 }
 
 func main() {
+	//open database
+	var err error
+	db, err = sql.Open("mysql", "root:120490Arkadi@tcp(172.31.130.134:3306)/hulton")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	//serves the static files in directory static
 	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/room_data/", room_data_fetcher)
+
 	//listens on port 8081
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
--- static/js/book.js
+++ static/js/book.js
@@ -1,5 +1,8 @@
-function getRoomByState() {
-	$.get("/room_data/", function(data) {
+function getRoomsByState() {
+	console.log("here")
+	var state = $('#room_state_selection').find(":selected").text();
+
+	$.get("/room_data/", {state: state}, function(data) {
 		for (room in data) {
 			var $room_entry = $($(".room_entry").html());
 			$('.room_entry_title', $room_entry).text(room.hotel_name);
@@ -9,4 +12,5 @@ function getRoomByState() {
 			$("#show_rooms").append(room_entry);
 		}
 	});
+	console.log("here 2")
 }
